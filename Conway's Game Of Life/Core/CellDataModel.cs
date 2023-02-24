95715d73c2a517224c67b4791b4e7e2ce3f640ca
Nathaniel R <32833323+CoolKidCrow@users.noreply.github.com>
2023-02-23 22:32:13 -0700

    Add project files.

---
+++ Conway's Game Of Life/Core/CellDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conway_s_Game_Of_Life.Core
+{
+    public class CellDataModel
+    {
+        public CellDataModel(object data)
+        {
+            this.Data = data;
+        }
+
+        public object Data { get; set; }
+    }
+}
---
+++ Conway's Game Of Life/MVVM/View.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Conway_s_Game_Of_Life
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
